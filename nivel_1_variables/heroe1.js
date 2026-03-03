81012b535ffe7bc399d8a60f557d589a79758081
John <aprendophpdays@gmail.com>
2026-03-03 06:50:40 -0500

    creo variables heroe

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -1,1 +1,12 @@
 //Heroe 1 
+let nombreHeroe = "WindowsDefender";
+let poder = "eliminar virus";
+let nivelEnergia = 85;
+let ciudad = "Windows";
+let enemigo = "Ramsomware";
+
+console.log("Nombre de heroe1: "+nombreHeroe);
+console.log("Tiene el poder de "+poder);
+console.log("Su nivel es: "+nivelEnergia);
+console.log("Vive en "+ciudad);
+console.log("Su enemigo es "+enemigo);   
