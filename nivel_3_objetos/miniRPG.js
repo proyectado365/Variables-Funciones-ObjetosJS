796a530c342fe034a5a351aa1d12bf0828785d3c
Camana Nick <camananick@gmail.com>
2026-03-03 23:36:01 -0500

    Fase 3 - Objetos

---
+++ nivel_3_objetos/miniRPG.js
@@ -0,0 +1,32 @@
+// MINI RPG
+
+let jugador = {
+    nombre: "Voltix",
+    vida: 100,
+    fuerza: 25,
+    nivel: 1
+};
+
+function atacar(jugador) {
+    console.log(jugador.nombre + " realiza un ataque.");
+    jugador.vida -= 10;
+}
+
+function subirNivel(jugador) {
+    console.log(jugador.nombre + " sube de nivel!");
+    jugador.nivel += 1;
+    jugador.fuerza += 5;
+}
+
+function mostrarJugador(jugador) {
+    console.log("Nombre: " + jugador.nombre);
+    console.log("Vida: " + jugador.vida);
+    console.log("Fuerza: " + jugador.fuerza);
+    console.log("Nivel: " + jugador.nivel);
+}
+
+// Simulación
+mostrarJugador(jugador);
+atacar(jugador);
+subirNivel(jugador);
+mostrarJugador(jugador);
---
+++ nivel_3_objetos/restaurante.js
@@ -0,0 +1,33 @@
+// SIMULADOR DE RESTAURANTE
+
+// Objeto cliente
+let cliente = {
+    nombre: "Carlos",
+    dinero: 50,
+    hambre: 80
+};
+
+// Función ordenar comida
+function ordenarComida(cliente) {
+    console.log(cliente.nombre + " está ordenando comida...");
+    cliente.hambre -= 40;
+}
+
+// Función pagar
+function pagar(cliente) {
+    console.log(cliente.nombre + " está pagando la cuenta...");
+    cliente.dinero -= 30;
+}
+
+// Función mostrar estado
+function mostrarEstado(cliente) {
+    console.log("Estado actual de " + cliente.nombre);
+    console.log("Dinero: " + cliente.dinero);
+    console.log("Hambre: " + cliente.hambre);
+}
+
+// Simulación
+mostrarEstado(cliente);
+ordenarComida(cliente);
+pagar(cliente);
+mostrarEstado(cliente);
