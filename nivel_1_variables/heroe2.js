92daf4ca6ff6d41061c11675364300384e10dbb9
Camana Nick <camananick@gmail.com>
2026-03-03 23:21:13 -0500

    Fase 1 - Variables - Heroe2

---
+++ nivel_1_variables/heroe2.js
@@ -0,0 +1,18 @@
+// FASE 1 - SOLO VARIABLES
+
+// -----------------------  
+
+// HEROE 2
+
+let nombreHeroe = "Voltix";
+let poder = "Controlar la electricidad";
+let nivelEnergia = 90;
+let ciudad = "Lima";
+let enemigo = "x";
+
+console.log("Nombre de heroe2: " + nombreHeroe);
+console.log("Tiene el poder de " + poder);
+console.log("Su nivel es: " + nivelEnergia);
+console.log("Defiende la ciudad de " + ciudad);
+console.log("Su enemigo es " + enemigo);
+console.log("La batalla está por comenzar...");
